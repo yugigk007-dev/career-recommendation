f4da070fdeea310cbcbda4e44cbdac70a486cbf7
Yugender <yugigk007@gmail.com>
2026-04-08 18:54:44 +0530

    prepare for deployment

--- dtproject/settings.py
+++ dtproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+@jvj&avv9a=7jt_flf@-%y%@&k1y62+=2cxer70*mkl#-w#oi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
